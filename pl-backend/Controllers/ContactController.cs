fce1a7e5a1f06fef77598829a7e5279f6c3074b7
KlizX <szymonTESTTEST155@gmail.com>
2023-06-05 19:07:36 +0200

    tmp 2

---
+++ pl-backend/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using pl_backend.Data;
+using pl_backend.DTO;
+using pl_backend.Models;
+using pl_backend.Services;
+using System.Data;
+
+namespace pl_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly DataContext DataContext;
+        private readonly ITokenService TokenService;
+        private readonly IContactService ContactService;
+
+        public ContactController(DataContext dataContext, ITokenService tokenService, IContactService contactService)
+        {
+            DataContext = dataContext;
+            TokenService = tokenService;
+            ContactService = contactService;
+        }
+    }
+}
---
+++ pl-backend/DTO/UserContactDto.cs
@@ -0,0 +1,9 @@
+﻿namespace pl_backend.DTO
+{
+    public class UserContactDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+    }
+}
